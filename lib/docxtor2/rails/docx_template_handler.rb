f7833485786bbbe1e1311b551f8ec05a43110d77
Vasiliy Yorkin <vyorkin@undev.ru>
2013-07-10 20:58:05 +0400

    rename stream to stringio

--- lib/docxtor2/rails/docx_template_handler.rb
+++ lib/docxtor2/rails/docx_template_handler.rb
@@ -2,8 +2,8 @@ module Docxtor2
   module Rails
     class DocxTemplateHandler < ActionView::TemplateHandler
       def render(template, local_assigns = {})
-        stream = ::Docxtor2.generate &(eval("proc {#{template.source}}"))
-        stream.string
+        stringio = ::Docxtor2.generate &(eval("proc {#{template.source}}"))
+        stringio.string
       end
     end
   end
