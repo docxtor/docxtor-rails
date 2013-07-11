c3af166fe7406d9dc25610bf891636d8fd91b28d
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2013-07-11 11:45:47 +0400

    simplify template handler spec

--- spec/docxtor2/rails/docx_template_handler_spec.rb
+++ spec/docxtor2/rails/docx_template_handler_spec.rb
@@ -4,17 +4,13 @@ module Docxtor2
   module Rails
     describe DocxTemplateHandler do
       let(:template) { ActionView::Template.new(File.join(Known::Path::SUPPORT, 'template.docx')) }
+      subject { DocxTemplateHandler.new.render(template) }
 
       it 'is registered' do
         ActionView::Template.registered_template_handler(:docx).should_not be_nil
       end
       
-      it 'should render template' do
-        actual = subject.render(template)
-
-        actual.should_not be_nil
-        actual.size.should be > 0
-      end
+      its(:size) { should be > 0 }
     end
   end
 end
